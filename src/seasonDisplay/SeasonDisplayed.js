a488fb726d811d37a87a82599163add0d4e817f0
motunrayo <akinrimisimotunrayo@gmail.com>
2021-01-13 12:53:33 +0100

    season displayed component

--- src/seasonDisplay/SeasonDisplayed.js
+++ src/seasonDisplay/SeasonDisplayed.js
@@ -8,25 +8,25 @@ const getSeason = (lat, month) => {
          return lat > 0 ? "winter" : "summer"
       }
 }
-// const seasonConfig = {
-//    summer : { 
-//       text: "it's a sunny season",
-//       iconName: "sun"
-//    },
-//    winter : {
-//       text: "it is a chilly season",
-//       iconName: "snowflake"
-//    }
-// }
+const seasonConfig = {
+   summer : { 
+      text: "it's a sunny season",
+      iconName: "sun"
+   },
+   winter : {
+      text: "it is a chilly season",
+      iconName: "snowflake"
+   }
+}
 
 const SeasonDisplayed = (props) => {
    const currentSeason = getSeason(props.lat, new Date().getMonth());
-   // const {text, iconName} = seasonConfig[currentSeason];
+   const {text, iconName} = seasonConfig[currentSeason];
    return (
       <div className={`season-displayed ${currentSeason}`}>
-         {/* <i className={`icon-left massive ${iconName} icon`}></i>
+         <i className={`icon-left massive ${iconName} icon`}></i>
                <h1>{text}</h1>
-         <i className={`icon-right massive ${iconName} icon`}></i> */}
+         <i className={`icon-right massive ${iconName} icon`}></i>
       </div>
    )
 }
